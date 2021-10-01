9119ab335d0efbceeb4d53c41c8dc2102dfde62d
quangnt1702 <quangnt1702@gmail.com>
2021-10-01 10:28:23 +0700

    Add Recursion

--- src/com/quangnt/mathutil/core/MathUtil.java
+++ src/com/quangnt/mathutil/core/MathUtil.java
@@ -16,18 +16,28 @@ public class MathUtil {
     //ko tinh dc am giai thua
     //0!=1!=1
     //TDD - Test Driven Development - viet test trc khi viet code
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException("n must between 0...20");
+//        
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+    //Recursion
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20) 
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must between 0...20");
-        
+        }
         if (n == 0 || n == 1) {
             return 1;
         }
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        return n * getFactorial(n - 1);
     }
 
 }
